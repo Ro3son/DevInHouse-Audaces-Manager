ea22a16ebcc80edf453bbceb17df025da5b4cb42
Robson De Andrade <theairelibre@protonmail.com>
2022-05-16 19:19:27 -0300

    import components

--- app-audaces-manager/src/app/app.module.ts
+++ app-audaces-manager/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component';
+import { HomeComponent } from './home/home/home.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    EsqueciSenhaComponent
+    EsqueciSenhaComponent,
+    HomeComponent,
+    DashboardComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
