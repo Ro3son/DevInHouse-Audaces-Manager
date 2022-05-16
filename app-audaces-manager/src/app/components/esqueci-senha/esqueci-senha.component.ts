698d94222b18aea2cd54b34fb5ab5bf4dbb386ce
Robson De Andrade <theairelibre@protonmail.com>
2022-05-16 15:55:17 -0300

    create variables FormBuilder Validators for email

---
+++ app-audaces-manager/src/app/components/esqueci-senha/esqueci-senha.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-esqueci-senha',
+  templateUrl: './esqueci-senha.component.html',
+  styleUrls: ['./esqueci-senha.component.scss']
+})
+export class EsqueciSenhaComponent {
+
+  public h1: string = 'Redefinir Senha';
+
+  public paragraph: string = 'Digite o e-mail associado à sua conta e enviaremos instruções para redefinir sua senha.'
+
+  public labelTextContent: string = 'Informe seu e-mail';
+
+  public buttonTextContent: string = 'Enviar Recuperação';
+
+  constructor(private fb: FormBuilder) { }
+
+  public form = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
+  });
+
+  public onSubmit() {
+    console.log(this.form.value);
+  }
+}
