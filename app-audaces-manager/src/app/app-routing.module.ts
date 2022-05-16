04b909fa7941580e9b050d965c8586fecb6637dd
Robson De Andrade <theairelibre@protonmail.com>
2022-05-16 19:18:04 -0300

    add route paths

--- app-audaces-manager/src/app/app-routing.module.ts
+++ app-audaces-manager/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component'
+import { DashboardComponent } from './home/dashboard/dashboard.component';
 
+// Path é o caminho (URL) e o componente //
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch:'full' },
   { path: 'login', component: LoginComponent },
   { path: 'senha', component: EsqueciSenhaComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
