1b6f99b031c7c929a44001a01f77accc141bfb3e
Robson De Andrade <theairelibre@protonmail.com>
2022-05-18 16:27:40 -0300

    public routing

---
+++ app-audaces-manager/src/app/modules/public/public-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from 'src/app/components/login/login.component';
+import { EsqueciSenhaComponent } from 'src/app/components/esqueci-senha/esqueci-senha.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'senha', component: EsqueciSenhaComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PublicRoutingModule {}
