538bd2357e46f34284e50896140ee91a54c2393c
Robson De Andrade <theairelibre@protonmail.com>
2022-05-19 16:08:42 -0300

    create c listar coleções

---
+++ app-audaces-manager/src/app/components/colecoes/listar-colecoes/listar-colecoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-colecoes',
+  templateUrl: './listar-colecoes.component.html',
+  styleUrls: ['./listar-colecoes.component.scss']
+})
+export class ListarColecoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
