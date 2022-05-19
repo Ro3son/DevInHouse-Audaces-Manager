af1bd22a0df7da27bbcbe07c79fef26de72f138f
Robson De Andrade <theairelibre@protonmail.com>
2022-05-19 16:13:32 -0300

    create service modelo

---
+++ app-audaces-manager/src/app/services/modelo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Modelos } from '../classes/modelos';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModeloService {
+
+  private requestURL = environment.Modelos_URL;
+
+  constructor(private http: HttpClient) { }
+
+  public getModelos(): Observable<Modelos[]> {
+    return this.http.get<Modelos[]>(this.requestURL);
+  }
+}
