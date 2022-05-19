f85f9a295b1024d327f873d1cfa82804f0a8c1c9
Robson De Andrade <theairelibre@protonmail.com>
2022-05-19 16:13:03 -0300

    create service colecao

---
+++ app-audaces-manager/src/app/services/colecao.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Colecoes } from '../classes/colecoes';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColecaoService {
+
+  private requestURL = environment.Colecoes_URL;
+
+  constructor(private http: HttpClient) { }
+
+  public getColecoes(): Observable<Colecoes[]> {
+    return this.http.get<Colecoes[]>(this.requestURL);
+  }
+}
