9444846805aab3979bc7f820f58006f05819e973
Robson De Andrade <theairelibre@protonmail.com>
2022-05-19 16:08:21 -0300

    create c cadastar coleção

---
+++ app-audaces-manager/src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-colecao',
+  templateUrl: './cadastrar-colecao.component.html',
+  styleUrls: ['./cadastrar-colecao.component.scss']
+})
+export class CadastrarColecaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
