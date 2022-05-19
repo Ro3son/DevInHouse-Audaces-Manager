d930d2b7f1e9349ff2c4103d1c7a5f59af5e605b
Robson De Andrade <theairelibre@protonmail.com>
2022-05-19 16:09:02 -0300

    create c editar coleção

---
+++ app-audaces-manager/src/app/components/colecoes/editar-colecao/editar-colecao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-colecao',
+  templateUrl: './editar-colecao.component.html',
+  styleUrls: ['./editar-colecao.component.scss']
+})
+export class EditarColecaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
