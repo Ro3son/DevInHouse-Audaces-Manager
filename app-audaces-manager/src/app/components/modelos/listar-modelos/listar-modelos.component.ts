7932b4db75fa857b21b0e60c8686ca457aac5613
Robson De Andrade <theairelibre@protonmail.com>
2022-05-21 20:58:52 -0300

    create c listar modelos

---
+++ app-audaces-manager/src/app/components/modelos/listar-modelos/listar-modelos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-modelos',
+  templateUrl: './listar-modelos.component.html',
+  styleUrls: ['./listar-modelos.component.scss']
+})
+export class ListarModelosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
