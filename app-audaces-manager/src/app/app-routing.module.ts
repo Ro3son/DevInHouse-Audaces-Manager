41219a5b00b4ef93ac5a4b8426545d25daf006e5
Robson De Andrade <theairelibre@protonmail.com>
2022-05-21 11:44:41 -0300

    change path home

--- app-audaces-manager/src/app/app-routing.module.ts
+++ app-audaces-manager/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   { path: '', redirectTo: 'public', pathMatch: 'full' },
   { path: 'public', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule) },
-  { path: 'private', loadChildren: () => import('./modules/private/private.module').then(p => p.PrivateModule) }
-
+  { path: 'home', loadChildren: () => import('./modules/private/private.module').then(p => p.PrivateModule) }
 ];
 
 @NgModule({
