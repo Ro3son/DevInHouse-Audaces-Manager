285fc8f8302dd3fe4b8e5c616e429c5ac31ced43
Robson De Andrade <theairelibre@protonmail.com>
2022-05-21 20:59:16 -0300

    routes path children c home

--- app-audaces-manager/src/app/app-routing.module.ts
+++ app-audaces-manager/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+
   { path: '', redirectTo: 'public', pathMatch: 'full' },
   { path: 'public', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule) },
-  { path: 'home', loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule) }
+
+  { path: '', component: HomeComponent,
+    children: [
+      { path: 'home', loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule) },
+    ]
+  }
 ];
 
 @NgModule({
