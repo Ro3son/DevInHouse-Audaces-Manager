c4034e5563f721514ba497ac1eb98a6a8ff62599
Robson De Andrade <theairelibre@protonmail.com>
2022-05-22 16:47:15 -0300

    input decorator

--- app-audaces-manager/src/app/components/dashboard/dashboard.component.ts
+++ app-audaces-manager/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Colecoes } from '../../classes/colecoes';
-import { ColecaoService } from '../../services/colecao.service';
+import { Component, OnInit, Input } from '@angular/core';
+
 import { Modelos } from '../../classes/modelos';
 import { ModeloService } from '../../services/modelo.service';
 
@@ -11,20 +10,14 @@ import { ModeloService } from '../../services/modelo.service';
 })
 export class DashboardComponent implements OnInit {
 
-  public colecao: Colecoes[] = [];
+  @Input() colecao = [{},{}];
 
   public modelo: Modelos[] = [];
 
-  constructor(private ColecaoService: ColecaoService, private ModeloService: ModeloService) { }
+  constructor(private ModeloService: ModeloService) { }
 
-  ngOnInit() { this.getDataColecoes(), this.getDataModelos() }
+  ngOnInit() { this.getDataModelos() }
 
-  public getDataColecoes() {
-    this.ColecaoService.getColecoes().subscribe((dados) => {
-      this.colecao = dados;
-      console.log(this.colecao);
-    });
-  }
   public getDataModelos() {
     this.ModeloService.getModelos().subscribe((dados) => {
       this.modelo = dados;
