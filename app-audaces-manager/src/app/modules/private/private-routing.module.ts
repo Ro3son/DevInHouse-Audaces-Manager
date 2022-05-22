e9a9e7870aa58cdaa6c4e6925052269365769dcc
Robson De Andrade <theairelibre@protonmail.com>
2022-05-21 21:01:20 -0300

    routes path children of home

--- app-audaces-manager/src/app/modules/private/private-routing.module.ts
+++ app-audaces-manager/src/app/modules/private/private-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from 'src/app/home/home.component';
+
+import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { ListarColecoesComponent } from 'src/app/components/colecoes/listar-colecoes/listar-colecoes.component';
+import { ListarModelosComponent } from 'src/app/components/modelos/listar-modelos/listar-modelos.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'colecoes', component: ListarColecoesComponent },
+  { path: 'modelos', component: ListarModelosComponent },
 ]
 
 @NgModule({
