9f52a756465f4703a996462b69c015349fb964c9
Robson De Andrade <theairelibre@protonmail.com>
2022-05-22 13:51:34 -0300

    import colecoes component

--- app-audaces-manager/src/app/app.module.ts
+++ app-audaces-manager/src/app/app.module.ts
@@ -7,28 +7,28 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './components/login/login.component';
 import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component';
+import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
 
 import { HttpClientModule } from '@angular/common/http';
+
+import { ColecoesComponent } from './components/colecoes/colecoes.component';
 import { ListarColecoesComponent } from './components/colecoes/listar-colecoes/listar-colecoes.component';
 import { CadastrarColecaoComponent } from './components/colecoes/cadastrar-colecao/cadastrar-colecao.component';
-import { EditarColecaoComponent } from './components/colecoes/editar-colecao/editar-colecao.component';
-import { ListarModelosComponent } from './components/modelos/listar-modelos/listar-modelos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     EsqueciSenhaComponent,
+    HomeComponent,
     MenuComponent,
     DashboardComponent,
-    HomeComponent,
+    ColecoesComponent,
     ListarColecoesComponent,
     CadastrarColecaoComponent,
-    EditarColecaoComponent,
-    ListarModelosComponent,
+  
   ],
   imports: [
     BrowserModule,
