6ed9eab95a24d334bde57bdb19d064b31704bc54
Robson De Andrade <theairelibre@protonmail.com>
2022-05-22 21:32:36 -0300

    add new path

--- app-audaces-manager/src/app/modules/private/private-routing.module.ts
+++ app-audaces-manager/src/app/modules/private/private-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { ListarColecoesComponent } from 'src/app/components/colecoes/listar-colecoes/listar-colecoes.component';
 import { ListarModelosComponent } from 'src/app/components/modelos/listar-modelos/listar-modelos.component';
+import { CadastrarColecaoComponent } from 'src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'colecoes', component: ListarColecoesComponent },
+  { path: 'criar', component: CadastrarColecaoComponent },
   { path: 'modelos', component: ListarModelosComponent },
 ]
 
