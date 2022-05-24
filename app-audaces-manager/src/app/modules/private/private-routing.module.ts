d4a3b90b8e962fd578b17a59c0bcbea046fa8931
Robson De Andrade <theairelibre@protonmail.com>
2022-05-24 15:28:49 -0300

    change path c colecoes

--- app-audaces-manager/src/app/modules/private/private-routing.module.ts
+++ app-audaces-manager/src/app/modules/private/private-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
-import { ListarColecoesComponent } from 'src/app/components/colecoes/listar-colecoes/listar-colecoes.component';
 import { ListarModelosComponent } from 'src/app/components/modelos/listar-modelos/listar-modelos.component';
 import { CadastrarColecaoComponent } from 'src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component';
+import { ColecoesComponent } from 'src/app/components/colecoes/colecoes.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'colecoes', component: ListarColecoesComponent },
+  { path: 'colecoes', component: ColecoesComponent },
   { path: 'criar', component: CadastrarColecaoComponent },
   { path: 'modelos', component: ListarModelosComponent },
 ]
