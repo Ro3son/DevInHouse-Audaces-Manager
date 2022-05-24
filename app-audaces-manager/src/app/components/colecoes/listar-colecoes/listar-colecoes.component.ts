62e6755751edc1fbf117f7622f22b5e69bf555ff
Robson De Andrade <theairelibre@protonmail.com>
2022-05-24 16:01:23 -0300

    remove ngOnInit

--- app-audaces-manager/src/app/components/colecoes/listar-colecoes/listar-colecoes.component.ts
+++ app-audaces-manager/src/app/components/colecoes/listar-colecoes/listar-colecoes.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Colecoes } from 'src/app/classes/colecoes';
-import { ColecaoService } from 'src/app/services/colecao.service';
 
 @Component({
   selector: 'app-listar-colecoes',
   templateUrl: './listar-colecoes.component.html',
   styleUrls: ['./listar-colecoes.component.scss'],
 })
-export class ListarColecoesComponent implements OnInit {
+export class ListarColecoesComponent {
 
   @Input() colecoes: Colecoes[] = [];
 
   constructor() {}
-
-  ngOnInit() { console.log(this.colecoes) }
  
 }
