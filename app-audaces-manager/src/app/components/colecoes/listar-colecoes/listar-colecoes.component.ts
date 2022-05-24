9899b82533b8655ee4234394a689fc45f7c028e0
Robson De Andrade <theairelibre@protonmail.com>
2022-05-23 21:30:02 -0300

    import class colecoes and service

--- app-audaces-manager/src/app/components/colecoes/listar-colecoes/listar-colecoes.component.ts
+++ app-audaces-manager/src/app/components/colecoes/listar-colecoes/listar-colecoes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Colecoes } from 'src/app/classes/colecoes';
+import { ColecaoService } from 'src/app/services/colecao.service';
 
 @Component({
   selector: 'app-listar-colecoes',
   templateUrl: './listar-colecoes.component.html',
-  styleUrls: ['./listar-colecoes.component.scss']
+  styleUrls: ['./listar-colecoes.component.scss'],
 })
 export class ListarColecoesComponent implements OnInit {
 
-  constructor() { }
+  public colecoes: Colecoes[] = [];
 
-  ngOnInit(): void {
+  constructor(private colecoesService: ColecaoService) {}
+
+  ngOnInit() { this.getDataColecoes() }
+ 
+  public getDataColecoes() {
+    this.colecoesService.getColecoes().subscribe((data) => {
+      this.colecoes = data.map((key) => {
+        return new Colecoes(key.id, key.nome, key.estacao, key.responsavel);
+      });
+    });
   }
-
 }
