a8740e63990c99bb9550b0a5e08c63d3588ad2b4
Robson De Andrade <theairelibre@protonmail.com>
2022-05-23 21:26:11 -0300

    add variables, methods and import service

--- app-audaces-manager/src/app/components/dashboard/dashboard.component.ts
+++ app-audaces-manager/src/app/components/dashboard/dashboard.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Colecoes } from 'src/app/classes/colecoes';
+import { ColecaoService } from 'src/app/services/colecao.service';
 import { Modelos } from '../../classes/modelos';
 import { ModeloService } from '../../services/modelo.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
 
-  @Input() colecao: Colecoes[] = [];
+  public totalColecao: Colecoes[] = [];
 
-  public modelo: Modelos[] = [];
+  public totalModelo: Modelos[] = [];
 
-  constructor(private modeloService: ModeloService) { }
+  constructor(
+    private modeloService: ModeloService,
+    private colecaoService: ColecaoService
+  ) { }
 
-  ngOnInit() { this.getDataModelos() }
+  ngOnInit() {
+    this.getDataModelos(), this.getDataColecoes();
+  }
 
   public getDataModelos() {
     this.modeloService.getModelos().subscribe((dados) => {
-      this.modelo = dados;
+      this.totalModelo = dados;
+      console.log(this.totalModelo);
+    });
+  }
+  public getDataColecoes() {
+    this.colecaoService.getColecoes().subscribe((dados) => {
+      this.totalColecao = dados;
+      console.log(this.totalColecao);
     });
   }
 }
