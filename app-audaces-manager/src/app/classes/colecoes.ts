c5eaa6c77bc3e7ebac68ff855b2f691ecbaa1e8f
Robson De Andrade <theairelibre@protonmail.com>
2022-05-25 10:10:03 -0300

    add property lancamento

--- app-audaces-manager/src/app/classes/colecoes.ts
+++ app-audaces-manager/src/app/classes/colecoes.ts
@@ -1,13 +1,15 @@
 export class Colecoes {
     id: number;
     nome: string;
-    estacao: string
+    estacao: string;
+    lancamento: number;
     responsavel: string;
 
-    constructor(id: number, nome: string, estacao: string, responsavel: string) {
+    constructor(id: number, nome: string, estacao: string, lancamento: number, responsavel: string) {
         this.id = id;
         this.nome = nome;
         this.estacao = estacao;
+        this.lancamento = lancamento;
         this.responsavel = responsavel;
     }
 }
