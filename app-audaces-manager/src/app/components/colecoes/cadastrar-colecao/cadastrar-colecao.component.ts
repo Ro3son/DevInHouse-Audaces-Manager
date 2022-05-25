8528ef76e3615b871d450a3f3b07c1174dd18cdd
Robson De Andrade <theairelibre@protonmail.com>
2022-05-25 12:03:13 -0300

    create variables and method

--- app-audaces-manager/src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component.ts
+++ app-audaces-manager/src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Colecoes } from 'src/app/classes/colecoes';
+import { ColecaoService } from 'src/app/services/colecao.service';
 
 @Component({
   selector: 'app-cadastrar-colecao',
   templateUrl: './cadastrar-colecao.component.html',
   styleUrls: ['./cadastrar-colecao.component.scss']
 })
-export class CadastrarColecaoComponent implements OnInit {
+export class CadastrarColecaoComponent {
 
-  constructor() { }
+  public cadastroDeColecao: Colecoes[] = [];
 
-  ngOnInit(): void {
+  public cadastraColecao: Colecoes = { id: 0, nome: '', estacao: '', lancamento: 0, responsavel: '' }
+
+  constructor(private colecaoService: ColecaoService) { }
+
+  public onSubmit() { }
+
+  public postDataColecoes() {
+    this.colecaoService.postColecoes(this.cadastraColecao).subscribe((dado) => {
+      this.cadastroDeColecao = dado.map((key) => {
+        return new Colecoes(key.id, key.nome, key.estacao, key.lancamento, key.responsavel);
+      });
+    });
   }
-
 }
