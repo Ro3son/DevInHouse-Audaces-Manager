6210012dedcac3cca594f8c4f6dcf8518feaa732
Robson De Andrade <theairelibre@protonmail.com>
2022-05-27 22:51:17 -0300

    create c cadastrar modelo

---
+++ app-audaces-manager/src/app/components/modelos/cadastrar-modelo/cadastrar-modelo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-modelo',
+  templateUrl: './cadastrar-modelo.component.html',
+  styleUrls: ['./cadastrar-modelo.component.scss']
+})
+export class CadastrarModeloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
