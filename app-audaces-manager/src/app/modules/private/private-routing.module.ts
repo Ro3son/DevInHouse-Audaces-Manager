1ec49e9aea8c5914d462409b76f7153c9e6515ec
Robson De Andrade <theairelibre@protonmail.com>
2022-05-28 21:11:55 -0300

    add new path modelos

--- app-audaces-manager/src/app/modules/private/private-routing.module.ts
+++ app-audaces-manager/src/app/modules/private/private-routing.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
-import { ListarModelosComponent } from 'src/app/components/modelos/listar-modelos/listar-modelos.component';
 import { CadastrarColecaoComponent } from 'src/app/components/colecoes/cadastrar-colecao/cadastrar-colecao.component';
 import { ColecoesComponent } from 'src/app/components/colecoes/colecoes.component';
 import { EditarColecaoComponent } from 'src/app/components/colecoes/editar-colecao/editar-colecao.component';
+import { ModelosComponent } from 'src/app/components/modelos/modelos.component';
+import { CadastrarModeloComponent } from 'src/app/components/modelos/cadastrar-modelo/cadastrar-modelo.component';
+import { EditarModeloComponent } from 'src/app/components/modelos/editar-modelo/editar-modelo.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'colecoes', component: ColecoesComponent },
   { path: 'cadastrar', component: CadastrarColecaoComponent },
   { path: 'colecoes/:colecaoId', component: EditarColecaoComponent },
-
-  { path: 'modelos', component: ListarModelosComponent },
+  { path: 'modelos', component: ModelosComponent },
+  { path: 'cadastrarModelos',component: CadastrarModeloComponent },
+  { path: 'editarModelos', component: EditarModeloComponent }
 ]
 
 @NgModule({
