205084fab51d4132904eb6e95584246627ff0d1c
Robson De Andrade <theairelibre@protonmail.com>
2022-05-29 11:34:18 -0300

    import modelo class, service

--- app-audaces-manager/src/app/components/modelos/cadastrar-modelo/cadastrar-modelo.component.ts
+++ app-audaces-manager/src/app/components/modelos/cadastrar-modelo/cadastrar-modelo.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Modelos } from 'src/app/classes/modelos';
+import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
   selector: 'app-cadastrar-modelo',
   templateUrl: './cadastrar-modelo.component.html',
   styleUrls: ['./cadastrar-modelo.component.scss']
 })
-export class CadastrarModeloComponent implements OnInit {
+export class CadastrarModeloComponent {
 
-  constructor() { }
+  public cadastroDeModelo: Modelos[] = [];
 
-  ngOnInit(): void {
+  public cadastraModelo: Modelos = { id: 0, nome: '', colecao: '', responsavel: '' };
+
+  constructor(private modeloService: ModeloService) { }
+
+  public postDataModelos() {
+    this.modeloService.postModelos(this.cadastraModelo).subscribe((dados) => {
+      this.cadastroDeModelo = dados.map((key) => {
+        return new Modelos(key.id, key.nome, key.colecao, key.responsavel);
+      });
+    });
   }
 
 }
