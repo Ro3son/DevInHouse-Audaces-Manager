1edb14cdd59caffabc281f774a7858b96bcbc84b
Robson De Andrade <theairelibre@protonmail.com>
2022-05-28 22:37:05 -0300

    change property colecao type string

--- app-audaces-manager/src/app/classes/modelos.ts
+++ app-audaces-manager/src/app/classes/modelos.ts
@@ -1,10 +1,10 @@
 export class Modelos {
     id: number;
     nome: string;
-    colecao: number;
+    colecao: string;
     responsavel: string;
 
-    constructor(id: number, nome: string, colecao: number, responsavel: string) {
+    constructor(id: number, nome: string, colecao: string, responsavel: string) {
         this.id = id;
         this.nome = nome;
         this.colecao = colecao;
