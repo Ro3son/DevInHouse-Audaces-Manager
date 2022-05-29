eea7b45434367b88c9e671a66733a7ad83cce4eb
Robson De Andrade <theairelibre@protonmail.com>
2022-05-29 15:48:03 -0300

    import class, service create HTTP delete

--- app-audaces-manager/src/app/components/modelos/editar-modelo/editar-modelo.component.ts
+++ app-audaces-manager/src/app/components/modelos/editar-modelo/editar-modelo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Modelos } from 'src/app/classes/modelos';
+import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
   selector: 'app-editar-modelo',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarModeloComponent implements OnInit {
 
-  constructor() { }
+  public edicaoDeModelos: Modelos[] = [];
 
-  ngOnInit(): void {
+  public editaModelo: Modelos = { id: 4, nome: '', colecao: '', responsavel: '' };
+
+  constructor(
+    private modeloService: ModeloService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const modelo_id = this.route.snapshot.paramMap;
+    const id = Number(modelo_id.get('modeloId'));
+    const modeloId = this.modeloService.goToId(id).subscribe();
   }
 
+  public editModelos() {
+    this.modeloService.putModelos(this.editaModelo).subscribe(() => {
+      alert('Modelo Alterado');
+    });
+  }
+  public deleteModelos(num: number) {
+    this.modeloService.deleteModelos(num).subscribe(() => {
+      alert('Modelo Deletado');
+    });
+  }
 }
